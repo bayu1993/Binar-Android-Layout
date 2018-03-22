62eac4dc4d011dbb5a71af9a1c72ee4e1569285f
bayu <bayuteguhpamuji@gmail.com>
2018-03-22 22:43:16 +0700

    [Bayu] add constraint layout

---
+++ app/src/main/java/io/github/bayu1993/learnlayout/ConstraintActivity.java
@@ -0,0 +1,16 @@
+package io.github.bayu1993.learnlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dell on 3/22/18.
+ */
+
+public class ConstraintActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
--- app/src/main/java/io/github/bayu1993/learnlayout/MainActivity.java
+++ app/src/main/java/io/github/bayu1993/learnlayout/MainActivity.java
@@ -1,13 +1,27 @@
 package io.github.bayu1993.learnlayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btn_setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_setting = (ImageButton) findViewById(R.id.btn_setting);
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSetting();
+            }
+        });
+    }
+    public void goToSetting(){
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
     }
 }
